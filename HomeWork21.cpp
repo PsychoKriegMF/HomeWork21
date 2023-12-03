73368cda8860049afa08809e63247cc1c186d32b
MishGun94 <kolczov9494@gmail.com>
2023-12-03 19:57:00 +0500

    Добавьте файлы проекта.

---
+++ HomeWork21.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+void p_swap(int* num1, int* num2) {
+	int tmp = *num1;
+	*num1 = *num2;
+	*num2 = tmp;
+}
+
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	std::cout << "Свап через указатели.\n";
+	int x, y;
+	std::cout << "Введите 2 числа -> ";
+	std::cin >> x >> y;
+	int* px = &x;
+	int* py = &y;
+	std::cout << "x = " << x << ",y = " << y << std::endl;
+	p_swap(px,py);
+	std::cout << "Махнули местами значения =)\n";
+	std::cout << "Теперь х = " << x << ", y = " << y << std::endl;
+
+
+	return 0;
+}
